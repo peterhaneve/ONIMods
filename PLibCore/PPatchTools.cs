e9af7604f2ea084d963cfe1aa43445867038af6b
Peter Han <peterhaneve@outlook.com>
2026-01-09 22:54:37 -0800

    Happy new year!

--- PLibCore/PPatchTools.cs
+++ PLibCore/PPatchTools.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2025 Peter Han
+ * Copyright 2026 Peter Han
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  * and associated documentation files (the "Software"), to deal in the Software without
  * restriction, including without limitation the rights to use, copy, modify, merge, publish,
